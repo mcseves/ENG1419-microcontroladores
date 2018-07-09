3e0ca074f4214159dc9d92c049b2e2e348ebf6e3
Willian Simões Pinto <>
2018-07-09 18:42:03 -0300

    Alterando o programa que envia a rotas

--- raspberry_pi/server_raspberry/envia_rota.py
+++ raspberry_pi/server_raspberry/envia_rota.py
@@ -1,15 +1,14 @@
 from app import consultar_rota
 import serial
 
-ser = serial.Serial('COM3', 9600)
+#ser = serial.Serial('COM4', 9600)
 rota = consultar_rota()
 list_coord = rota[0]["coordenadas"]
-str_rota = ''
+str_rota = str(len(list_coord)) + ','
 
 for coord in list_coord:
-    str_rota += str(coord['lat']) + ',' + str(coord['lng'])
+    str_rota += str(coord['lat']) + ',' + str(coord['lng']) + ','
 
-str_rota = ',' + str(len(list_coord))
 str_rota += '\n'
 
 byte_coord = str_rota.encode('utf-8')
